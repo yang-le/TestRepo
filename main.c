8b951aef32a1294d8ec414f2ed357173aeb48f54
yangle <qtxzh0125@gmail.com>
2014-02-23 10:45:49 +0800

    init commit

---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+ * main.c
+ *
+ *  Created on: 2014年2月22日
+ *      Author: yangle
+ */
+
+#include <stdio.h>
+#include <gl/glut.h>
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc,argv);
+    //显示模式初始化
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
+    //定义窗口大小
+    glutInitWindowSize(300,300);
+    //定义窗口位置
+    glutInitWindowPosition(100,100);
+    //创建窗口
+    glutCreateWindow("testgl");
+
+    const char* version = (const char*)glGetString(GL_VERSION);
+    printf("OpenGL 版本：%s\n", version);
+
+    glutMainLoop();
+    return 0;
+}
